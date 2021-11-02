0ae5c979a6f9eb6380158d21fde7954e6568b758
hirnimeshrampuresoftware <71631645+hirnimeshrampuresoftware@users.noreply.github.com>
2021-11-02 18:39:39 +0530

    Update pre-build-script-manylinux.sh

--- .github/workflows/pre-build-script-manylinux.sh
+++ .github/workflows/pre-build-script-manylinux.sh
@@ -14,7 +14,7 @@ make
 make install
 bison --version
 if [ `uname -m` == "aarch64" ]; then
-   for ver in {cp36-cp36m, cp37-cp37m, cp38-cp38, cp39-cp39} 
+   for ver in {cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39} 
     do
       /opt/python/$ver/bin/python -m pip install cython
    done
